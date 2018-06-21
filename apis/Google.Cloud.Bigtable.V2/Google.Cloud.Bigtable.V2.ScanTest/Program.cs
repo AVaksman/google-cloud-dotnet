60bc6203041fd62524948c5c23e197ec2ffbdc0b
Alex <avaksman@gmail.com>
2018-06-21 14:58:53 -0400

    Adding ScanTest and ReadWriteTests

---
+++ apis/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.ScanTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Google.Cloud.Bigtable.V2.ScanTest.Runner;
+
+namespace Google.Cloud.Bigtable.V2.ScanTest
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var appService = serviceProvider.GetService<App>();
+            if (!appService.ConfigureParser(args))
+            {
+                await appService.Run().ConfigureAwait(false);
+            }
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            // add configured instance of logging
+            serviceCollection.AddSingleton(new LoggerFactory()
+                .AddConsole()
+                .AddDebug());
+
+            // add logging
+            serviceCollection.AddLogging();
+
+            // build configuration
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+            serviceCollection.AddOptions();
+            serviceCollection.Configure<AppSettings>(configuration.GetSection("Configuration"));
+
+            // add services 
+            serviceCollection.AddTransient<IScanRunnerService, ScanRunnerService>();
+
+            // add app
+            serviceCollection.AddTransient<App>();
+
+            serviceCollection.AddSingleton<IConfiguration>(configuration);
+        }
+
+    }
+}
---
+++ apis/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.ScanTest/Runner/IScanRunnerService.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Google.Cloud.Bigtable.V2;
+using HdrHistogram;
+using System;
+using Google.Cloud.Bigtable.Common.V2;
+
+namespace Google.Cloud.Bigtable.V2.ScanTest.Runner
+{
+    public interface IScanRunnerService
+    {
+        long ReadErrors { get; set; }
+        ReadRowsRequest ReadRowsRequestBuilder(BigtableByteString rowKey);
+        void WriteCsvToConsole(TimeSpan scanDuration, int rowsRead,  LongConcurrentHistogram hScan);
+
+        void WriteCsv(TimeSpan scanDuration, int rowsRead,  LongConcurrentHistogram hScan);
+
+        Task<int> Scan(LongConcurrentHistogram histogramScan);
+    }
+}
